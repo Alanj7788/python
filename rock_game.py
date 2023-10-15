8d988c5164f5ada6c5bc699a971c8d3680c963dd
Alan Jose <alan.cse21@jecc.ac.in>
2023-10-15 13:38:02 +0530

    Second commit

--- rock_game.py
+++ rock_game.py
@@ -7,3 +7,27 @@ def get_choices():
     choices={"player":player_choice,"computer":computer_choice}
     return choices
 
+def check_win(player,computer):
+    print(f"You chose {player},Computer chose {computer}")
+    if player==computer:
+        return "its a tie"
+    elif player=="rock":
+        if computer=="scissor":
+            return "You win"
+        else:
+            return "You lose"
+    elif player=="paper":
+        if computer=="scissor":
+            return "You lose"
+        else:
+            return "You win"
+    else:
+        if computer=="paper":
+            return "You win"
+        else:
+            return "You lose"
+        
+choices=get_choices()
+result=check_win(choices["player"],choices["computer"])
+print(result)
+    
